778b035ac9cdb683e9157acc4aacbd976c4bd932
4R4S8 <4R4S8@EYZEE>
2024-02-09 19:14:00 +0330

    Using Linq to find numbers from an array that are greater than 5

---
+++ FirstTryInLINQ/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] Numbers = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+var query =
+    from num in Numbers
+    where num > 5
+    select num;
+
+foreach (int n in query)
+{
+    Console.WriteLine(n);
+}
